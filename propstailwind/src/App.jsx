3bae8bed78733684788c77fcc1101332d6c648be
latikaguptak <08guptal@gmail.com>
2024-05-05 21:29:41 +0530

    Props & tailwind application

---
+++ propstailwind/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Form from './assets/Component/Form'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-rose-500 text-2xl font-semibold bg-lime-300 py-5 rounded-2xl'> Latika with Code </h1>
+    <Form />
+    </>
+  )
+}
+
+export default App
