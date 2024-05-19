096d4267c872330a636d4b46248dfa9dc501dbf9
latikaguptak <08guptal@gmail.com>
2024-05-19 23:27:15 +0530

    update React_router files

--- React_router/src/App.jsx
+++ React_router/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Home from './Components/Home/Home'
+import Header from './Components/Header/Header'
+import About from './Components/About/About'
+import Footer from './Components/Footer/Footer'
 
 function App() {
-  const createBrowserRouter = () => {
+  // const createBrowserRouter = () => {
 
-  }
+  // }
   return (
     <>
-    
+    <Header/>
+    <Home/>
+    <About/>
+    <Footer/>
     </>
   )
 }
--- React_router/src/main.jsx
+++ React_router/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { RouterProvider } from 'react-router-dom'
+import Header from './Components/Header/Header.jsx'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+    errorElement: <h1>404 Not Found</h1>,
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
